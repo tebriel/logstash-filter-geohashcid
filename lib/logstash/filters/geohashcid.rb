079d5cf5b5a5125136e0dba5ab74c78066e9de5f
Chris Moultrie <cmoultrie@pindropsecurity.com>
2015-02-10 14:59:15 -0500

    Better safety..probably

--- lib/logstash/filters/geohashcid.rb
+++ lib/logstash/filters/geohashcid.rb
@@ -34,6 +34,9 @@ class LogStash::Filters::GeoHashCid < LogStash::Filters::Base
     # This filter needs to keep state.
     if @lcg_data.nil?
       lcg_file = ::File.join(::File.expand_path("../../../vendor/", ::File.dirname(__FILE__)), "lcg.json")
+      if !File.exists(lcg_file)
+        raise "Couldn't find the lcg.json (I looked for '#{lcg_file}'"
+      end
       @lcg_data = JSON.parse(lcg_file)
       @average_time = 0
     end
